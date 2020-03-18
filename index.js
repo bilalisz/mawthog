717a6eec6d817cd64cbe77a0628958a489a88e74
khantech166 <khan.tech885@gamil.com>
2020-03-18 14:12:33 +0500

    ejs clear

--- index.js
+++ index.js
@@ -1,12 +1,62 @@
 const express = require('express');
-const app = express();
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const pool = mysql.createConnection(
 
-const request = (req, res)=>{
-    res.send('This is my first api');
+    {
+        host     : 'localhost',
+        user     : 'root',
+        password : '',
+        database : 'eyeshop'
+      }
+
+);
+pool.connect();
+
+const app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const persons = [
+    {
+        id:1,
+        name:'Bilal'
+    },
+    {
+        id:2,
+        name:'Haidar'
+    }
+]
+const home = (req, res)=>{
+    pool.query('select * from wp_posts', (error, result, fields)=>{
+        console.log(result);
+        res.render('index', {data: result});
+    })
+
 }
 
+const about = (req, res)=>{
+    res.render('about')
+}
+app.set('view engine', 'ejs');
 
-app.use('/', request);
+app.get('/home', home);
+
+app.get('/about', about);
+
+
+const postAbout = (req, res)=>{
+    const message = req.body.message;
+    const country = req.body.country;
+    const posts = {message:message, country:country};
+    pool.query(`insert into cat set ?`, posts, (error,result)=>{
+        console.log(error)
+     res.send('done' + error);
+    });
+    
+}
+app.post('/postdata', postAbout);
+
 
 
 const port = 5000;
